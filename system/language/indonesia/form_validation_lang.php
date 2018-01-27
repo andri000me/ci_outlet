0265780f69a82c996d626df1070d6f8f0d2f59bc
Remithzu <remithzu@hotmail.com>
2018-01-27 10:52:53 +0700

    Update

---
+++ system/language/indonesia/form_validation_lang.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP
+ *
+ * This content is released under  MIT License (MIT)
+ *
+ * Copyright (c) 2014 - 2015, British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files ( "Software"), to deal
+ * in  Software without restriction, including without limitation  rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of  Software, and to permit persons to whom  Software is
+ * furnished to do so, subject to  following conditions:
+ *
+ *  above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of  Software.
+ *
+ *  SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO  WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OR
+ * LIABILITY, WHER IN AN ACTION OF CONTRACT, TORT OR ORWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH  SOFTWARE OR  USE OR OR DEALINGS IN
+ *  SOFTWARE.
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (http://ellislab.com/)
+ * @copyright	Copyright (c) 2014 - 2015, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	http://codeigniter.com
+ * @since	Version 1.0.0
+ * @filesource
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['form_validation_required']		= '{field} Harus diisi.';
+$lang['form_validation_isset']			= '{field} Harus memiliki Nilai.';
+$lang['form_validation_valid_email']		= '{field} Harus berisi alamat e-mail yang berlaku.';
+$lang['form_validation_valid_emails']		= '{field} Harus berisi semua alamat e-mail yang berlaku.';
+$lang['form_validation_valid_url']		= '{field} URL Harus Berisi Yang berlaku.';
+$lang['form_validation_valid_ip']		= '{field} Harus Berisi IP Yang berlaku.';
+$lang['form_validation_min_length']		= '{field} Harus minimal {param} Karakter.';
+$lang['form_validation_max_length']		= '{field} Tidak dapat melebihi {param} Karakter.';
+$lang['form_validation_exact_length']		= '{field} harus tepat {param} karakter.';
+$lang['form_validation_alpha']			= '{field} hanya dapat berisi karakter abjad.';
+$lang['form_validation_alpha_numeric']		= '{field} hanya dapat berisi karakter alpha - numerik.';
+$lang['form_validation_alpha_numeric_spaces']	= '{field} hanya dapat berisi karakter alpha - numerik dan spasi.';
+$lang['form_validation_alpha_dash']		= '{field} hanya dapat berisi karakter alpha - numeric, garis bawah, dan garis.';
+$lang['form_validation_numeric']		= '{field} harus berisi angka.';
+$lang['form_validation_is_numeric']		= '{field} harus berisi karakter numerik.';
+$lang['form_validation_integer']		= '{field} harus berisi integer.';
+$lang['form_validation_regex_match']		= '{field} tidak dalam format yang benar.';
+$lang['form_validation_matches']		= '{field} tidak sesuai dengan {param}.';
+$lang['form_validation_differs']		= '{field} harus berbeda dari {param}.';
+$lang['form_validation_is_unique'] 		= '{field} harus berisi nilai unik.';
+$lang['form_validation_is_natural']		= '{field} harus berisi angka.';
+$lang['form_validation_is_natural_no_zero']	= '{field} harus berisi angka dan harus lebih besar dari nol.';
+$lang['form_validation_decimal']		= '{field} harus berisi angka desimal.';
+$lang['form_validation_less_than']		= '{field} harus berisi angka kurang dari {param}.';
+$lang['form_validation_less_than_equal_to']	= '{field} harus berisi angka kurang dari atau sama dengan {param}.';
+$lang['form_validation_greater_than']		= '{field} harus berisi jumlah yang lebih besar dari {param}.';
+$lang['form_validation_greater_than_equal_to']	= '{field} harus berisi angka yang lebih besar dari atau sama dengan {param}.';
+$lang['form_validation_error_message_not_set']	= 'Tidak dapat mengakses pesan kesalahan yang sesuai dengan {field}.';
+$lang['form_validation_in_list']		= '{field} harus menjadi salah satu dari: {Param}.';
---
+++ system/language/indonesia/upload_lang.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014 - 2015, British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (http://ellislab.com/)
+ * @copyright	Copyright (c) 2014 - 2015, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	http://codeigniter.com
+ * @since	Version 1.0.0
+ * @filesource
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['upload_userfile_not_set'] = 'Tidak dapat menemukan variabel pos disebut userfile.';
+$lang['upload_file_exceeds_limit'] = 'Berkas yang diunggah melebihi ukuran maksimum yang diizinkan dalam file konfigurasi PHP Anda.';
+$lang['upload_file_exceeds_form_limit'] = 'Berkas yang diunggah melebihi ukuran maksimum yang diperbolehkan oleh formulir pengajuan.';
+$lang['upload_file_partial'] = 'File hanya sebagian upload.';
+$lang['upload_no_temp_directory'] = 'Folder temporary hilang.';
+$lang['upload_unable_to_write_file'] = 'File tidak bisa ditulis ke disk.';
+$lang['upload_stopped_by_extension'] = 'File upload dihentikan oleh ekstensi.';
+$lang['upload_no_file_selected'] = 'Anda tidak memilih file untuk di-upload.';
+$lang['upload_invalid_filetype'] = 'Tipe file yang Anda coba upload tidak diperbolehkan.';
+$lang['upload_invalid_filesize'] = 'File yang Anda coba upload lebih besar dari ukuran yang diizinkan.';
+$lang['upload_invalid_dimensions'] = 'Gambar yang Anda coba upload tidak sesuai dengan dimensi yang diperbolehkan.';
+$lang['upload_destination_error'] = 'Ada masalah saat mencoba untuk memindahkan file upload ke tujuan akhir.';
+$lang['upload_no_filepath'] = 'Folder upload tujuan tidak valid.';
+$lang['upload_no_file_types'] = 'Anda tidak memiliki jenis file yang diperbolehkan.';
+$lang['upload_bad_filename'] = 'Nama file yang Anda kirimkan sudah ada pada server.';
+$lang['upload_not_writable'] = 'Folder upload tujuan tidak dapat untuk ditulis.';
